9ff0cbed5a1ba9f496d301931bdeafea18084981
LaureMarchal <laure.marchal@etu.umontpellier.fr>
2018-10-09 01:17:37 +0200

    tested isValidPlaceForShip in grid test

--- src/test/scala/GridTest.scala
+++ src/test/scala/GridTest.scala
@@ -1,6 +1,6 @@
 import grid.Grid
 import grid.CaseType
-import ship.Position
+import ship.{Position, Ship}
 import org.scalatest._
 
 class GridTest extends FlatSpec with Matchers {
@@ -25,5 +25,25 @@ class GridTest extends FlatSpec with Matchers {
   }
 
 
+  "isValidPlaceForShip" should "true if a ship can be placed on those positions" in {
+    val emptyShipsGrid = Grid(List.fill(10)(List.fill(10)(CaseType.W)))
+    val fullGrid = Grid(List.fill(10)(List.fill(10)(CaseType.S)))
+
+    val lineToUpdate = fullGrid.grid(4).updated(2, CaseType.W)
+    val fullExceptOne_Grid = Grid(fullGrid.grid.updated(4,lineToUpdate))
+    val lineToUpdate2 = fullExceptOne_Grid.grid(4).updated(3, CaseType.W)
+    val fullExceptTwo_Grid = Grid(fullExceptOne_Grid.grid.updated(4,lineToUpdate2))
+
+    val lineToUpdate3 = emptyShipsGrid.grid(4).updated(2, CaseType.S)
+    val emptyExceptOne_Grid = Grid(emptyShipsGrid.grid.updated(4,lineToUpdate3))
+
+    val positions = List(Position(4, 2), Position(4, 3))
+
+    emptyShipsGrid.isValidPlaceForShip(positions) should be (true)
+    fullGrid.isValidPlaceForShip(positions) should be (false)
+
+    fullExceptTwo_Grid.isValidPlaceForShip(positions) should be (true)
+    emptyExceptOne_Grid.isValidPlaceForShip(positions) should be (false)
+  }
 
 }
