e9dd4497ea32d95821fd5b71b2b06b4c881ce73d
LaureMarchal <laure.marchal@etu.umontpellier.fr>
2018-10-09 01:09:47 +0200

    wrote test on Grid

--- src/test/scala/GridTest.scala
+++ src/test/scala/GridTest.scala
@@ -1,3 +1,29 @@
-class GridTest {
+import grid.Grid
+import grid.CaseType
+import ship.Position
+import org.scalatest._
+
+class GridTest extends FlatSpec with Matchers {
+
+  "isValidPositionForShip" should "true if no ship there yet at the tested position" in {
+    val emptyShipsGrid = Grid(List.fill(10)(List.fill(10)(CaseType.W)))
+    val fullGrid = Grid(List.fill(10)(List.fill(10)(CaseType.S)))
+
+    val lineToUpdate = fullGrid.grid(4).updated(2, CaseType.W)
+    val fullExceptOne_Grid = Grid(fullGrid.grid.updated(4,lineToUpdate))
+
+    val lineToUpdate2 = emptyShipsGrid.grid(4).updated(2, CaseType.S)
+    val emptyExceptOne_Grid = Grid(emptyShipsGrid.grid.updated(4,lineToUpdate2))
+
+    val position = Position(4, 2)
+
+    emptyShipsGrid.isValidPositionForShip(position) should be (true)
+    fullGrid.isValidPositionForShip(position) should be (false)
+
+    fullExceptOne_Grid.isValidPositionForShip(position) should be (true)
+    emptyExceptOne_Grid.isValidPositionForShip(position) should be (false)
+  }
+
+
 
 }
