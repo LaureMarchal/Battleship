e867176923f914decc1a64674f4c399296346362
LaureMarchal <laure.marchal@etu.umontpellier.fr>
2018-10-09 16:28:26 +0200

    fixed problems

--- src/test/scala/MainTest.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest._
-
-class MainTest extends FunSuite with DiagrammedAssertions {
-  /*test("Battleship should start with B") {
-    assert("Battleship".startsWith("B"))
-  }*/
-}
